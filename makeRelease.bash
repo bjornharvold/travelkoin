4f8b38542034fb46e283ca7c915a1efb2ff76e86
Bjorn Harvold <bjorn@harvold.com>
2018-02-02 16:54:53 +0700

    Adding release script

--- makeRelease.bash
+++ makeRelease.bash
@@ -22,13 +22,13 @@ cd ..
 echo "Committing minor version changes"
 git commit -m "Getting ready for another release"
 
-git flow release start $CURRENT_VERSION
-git flow release finish -m $CURRENT_VERSION $CURRENT_VERSION
+#git flow release start $CURRENT_VERSION
+#git flow release finish -m $CURRENT_VERSION $CURRENT_VERSION
 
-git push
-git push --tags
+#git push
+#git push --tags
 
-git checkout develop
+#git checkout develop
 
 echo "Enabling git messages for a release again"
 export GIT_MERGE_AUTOEDIT=yes
