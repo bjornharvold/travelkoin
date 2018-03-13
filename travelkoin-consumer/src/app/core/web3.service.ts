85e6e90cc0d8b63e158e8aa5dd456ce16521072c
Bjorn Harvold <bjorn@harvold.com>
2018-03-13 15:27:42 +0700

    API-79 More logging

--- travelkoin-consumer/src/app/core/web3.service.ts
+++ travelkoin-consumer/src/app/core/web3.service.ts
@@ -13,6 +13,7 @@ export class Web3Service {
     private _provider: string;
 
     weiToEther(value: BigNumber): BigNumber {
+        console.log(`value is : |${value}|`);
         console.log(`value is BigNumber: ${value instanceof BigNumber}`);
         console.log(`value is string: ${typeof value === 'string'}`);
         console.log(`value is object: ${typeof value === 'object'}`);
@@ -27,16 +28,17 @@ export class Web3Service {
         let result: BigNumber = null;
         if (value != null) {
             const theNumber: any = this.getW3().web3.fromWei(value, 'ether');
-
-            console.log(`weiToEther result is BigNumber: ${result instanceof BigNumber}`);
-            console.log(`weiToEther result is string: ${typeof result === 'string'}`);
-            console.log(`weiToEther result: ${result}`);
+            console.log(`theNumber: |${theNumber}|`);
 
             if (typeof theNumber  === 'string') {
                 result = new BigNumber(theNumber);
             } else {
                 result = theNumber;
             }
+
+            console.log(`weiToEther result is BigNumber: ${result instanceof BigNumber}`);
+            console.log(`weiToEther result is string: ${typeof result === 'string'}`);
+            console.log(`weiToEther result: |${result}|`);
         }
 
         return result;
